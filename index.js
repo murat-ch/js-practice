b70660d3c99c046a401342b05a4dfcaa850d9635
murat-ch <muratik.ch@gmail.com>
2020-11-18 07:38:22 -0500

    second-cheapest wine

--- index.js
+++ index.js
@@ -20,6 +20,46 @@ function countTrue(arr) {
     return counter;
 }
 
-countTrue([true, false, false, true, false])
-countTrue([false, false, false, false])
-countTrue([])
+// countTrue([true, false, false, true, false])
+// countTrue([false, false, false, false])
+// countTrue([])
+
+function joinDigits(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+        let strArr = `${i}`.split('');
+        arr.push(...strArr);
+    }
+    return arr.join('-');
+}
+
+// console.log(joinDigits(15));
+
+/**
+ * Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine. However, he has little
+ * knowledge of how to choose a good bottle. Being a very frugal gentleman (yet disliking looking like a cheapskate),
+ * he decides to use a very simple rule. In any selection of two or more wines, he will always buy the second-cheapest.
+   Given an array of wine objects, write a function that returns the name of the wine he will buy for the party.
+   If given an empty array, return null. If given an array of only one, Atticus will buy that wine.
+ * @param wines [{name: 'Wine A', price: 8}, ]
+ */
+function chosenWine(wines) {
+    if (wines.length === 0) return null;
+    if (wines.length === 1) return wines[0].name;
+    wines.sort((a, b) => {
+        return a.price - b.price;
+    });
+    return wines[1].name;
+
+}
+/*
+chosenWine([
+  { name: "Wine A", price: 8.99 },
+  { name: "Wine 32", price: 13.99 },
+  { name: "Wine 9", price: 10.99 }
+]) ➞ "Wine 9"
+
+chosenWine([{ name: "Wine A", price: 8.99 }]) ➞ "Wine A"
+
+chosenWine([]) ➞ null
+ */
